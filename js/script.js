9284d58280c562a7b75a27cb6b15201e388bc6d1
elenhoriun <elenhoruin@gmail.com>
2017-09-26 22:11:25 +0300

    Changed the function of switching content tabs

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
+
 $(document).ready(function(){
 	$('.ba-card__tab').on('click', function(){
 		$('.ba-card__tab').siblings().addClass('ba-card__tab--nonactive');
 		$(this).removeClass('ba-card__tab--nonactive');
-	});
 
-	$('.ba-card__info').on('click', function(){
-		$('.ba-card__content-tab').hide();
-		$('.ba-card__content-info').show();
-	});
-
-	$('.ba-card__ratings').on('click', function(){
-		$('.ba-card__content-tab').hide();
-		$('.ba-card__content-ratings').show();
-	});
-
-	$('.ba-card__buy').on('click', function(){
-		$('.ba-card__content-tab').hide();
-		$('.ba-card__content-buy').show();
-	});
-
-	// $('.ba-card__tab').on('click', function(){
-	// 	var headTab = $(this).data();
-	// 	console.log(headTab);
+		var $headTab = $(this).data('type');
+		// console.log($headTab);
 		
+		$('.ba-card__content-tab').hide();
+		$('.ba-card__content-tab[data-type="' + $headTab + '"]').show();
+		
+	});
+
+	// $('.ba-card__info').on('click', function(){
 	// 	$('.ba-card__content-tab').hide();
-	// 	$('.ba-card__content-tab[data-type^=" ' + headTab + ' "]').show();
-	
+	// 	$('.ba-card__content-info').show();
 	// });
+
+	// $('.ba-card__ratings').on('click', function(){
+	// 	$('.ba-card__content-tab').hide();
+	// 	$('.ba-card__content-ratings').show();
+	// });
+
+	// $('.ba-card__buy').on('click', function(){
+	// 	$('.ba-card__content-tab').hide();
+	// 	$('.ba-card__content-buy').show();
+	// });
+
 });
+
